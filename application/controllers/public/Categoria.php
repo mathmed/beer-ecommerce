cb7a6f0248555ad342bade5951b0f4cef1599670
eduviictor <eduvictornobrega@gmail.com>
2018-11-20 16:15:14 -0300

    Adicionando pagina categoria dinamica e link no menu apenas para pagina home

---
+++ application/controllers/public/Categoria.php
@@ -0,0 +1,28 @@
+<?php 
+
+/* Não permitindo que a URL seja acessada diretamente */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+Class Categoria extends CI_Controller{
+
+    public function __construct(){
+
+        /* carregando os models e DAOs necessários */
+		parent::__construct();
+        $this->load->dao("categoria_dao", "", TRUE);
+        $this->load->dao("bebida_dao", "", TRUE);
+    }
+
+    public function index(){
+        $data["nome_categoria"] = $_GET["categoria"];
+        $data["dados"] = $this->categoria_dao->getCategorias();
+        $data["categoria"] = $this->bebida_dao->getBebidas($data["nome_categoria"]);
+        $this->load->view("public/header.php");
+        $this->load->view("public/categoria.php", $data);
+        $this->load->view("public/footer.php");
+    }
+
+}
+
+
+?>
---
+++ application/views/public/categoria.php
@@ -0,0 +1,91 @@
+
+<?php print_r($categoria); ?>
+
+<!-- products-breadcrumb -->
+<div class="products-breadcrumb">
+		<div class="container">
+			<ul>
+				<li><i class="fa fa-home" aria-hidden="true"></i><a href="/beer-ecommerce/public/index">Home</a><span>|</span></li>
+				<li><?php echo $nome_categoria; ?></li>
+			</ul>
+		</div>
+	</div>
+<!-- //products-breadcrumb -->
+<!-- banner -->
+	<div class="banner">
+		<div class="w3l_banner_nav_left">
+			<nav class="navbar nav_bottom">
+			 <!-- Brand and toggle get grouped for better mobile display -->
+			  <div class="navbar-header nav_2">
+				  <button type="button" class="navbar-toggle collapsed navbar-toggle1" data-toggle="collapse" data-target="#bs-megadropdown-tabs">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				  </button>
+			   </div> 
+			   <!-- Collect the nav links, forms, and other content for toggling -->
+				<div class="collapse navbar-collapse" id="bs-megadropdown-tabs">
+					<ul class="nav navbar-nav nav_1">
+						<?php foreach($dados as $dado){ ?>
+							<li><a href="/beer-ecommerce/public/"><?php echo $dado['descricao_categoria'] ?></a></li>
+						<?php } ?>
+					</ul>
+				 </div><!-- /.navbar-collapse -->
+			</nav>
+		</div>
+		<div class="w3l_banner_nav_right">
+			<div class="w3l_banner_nav_right_banner9 w3l_banner_nav_right_banner_pet">
+				<h4>Your Pet Favourite Food</h4>
+				<p>Sint occaecat cupidatat non proident</p>
+				<a href="/beer-ecommerce/public/single">Shop Now</a>
+			</div>
+			<div class="w3ls_w3l_banner_nav_right_grid w3ls_w3l_banner_nav_right_grid_sub">
+				<h3 class="w3l_fruit"><?php echo $nome_categoria; ?></h3>
+				<div class="w3ls_w3l_banner_nav_right_grid1 w3ls_w3l_banner_nav_right_grid1_veg">
+
+                    <?php foreach($categoria as $item){ ?>
+                        <div class="col-md-3 w3ls_w3l_banner_left w3ls_w3l_banner_left_asdfdfd">
+                            <div class="hover14 column">
+                            <div class="agile_top_brand_left_grid w3l_agile_top_brand_left_grid">
+                                <div class="agile_top_brand_left_grid_pos">
+                                    <img src="<?=base_url("assets/public/images/offer.png")?>" alt=" " class="img-responsive" />
+                                </div>
+                                <div class="agile_top_brand_left_grid1">
+                                    <figure>
+                                        <div class="snipcart-item block">
+                                            <div class="snipcart-thumb">
+                                                <a href="/beer-ecommerce/public/single"><img src="<?=base_url("assets/public/images/57.png")?>" alt=" " class="img-responsive" /></a>
+                                                <p><?php echo $item['nome_bebida']; ?></p>
+                                                <h4><?php echo $item['preco_bebida']; ?><span>$10.00</span></h4>
+                                            </div>
+                                            <div class="snipcart-details">
+                                                <form action="#" method="post">
+                                                    <fieldset>
+                                                        <input type="hidden" name="cmd" value="_cart" />
+                                                        <input type="hidden" name="add" value="1" />
+                                                        <input type="hidden" name="business" value=" " />
+                                                        <input type="hidden" name="item_name" value="can - tuna for cats" />
+                                                        <input type="hidden" name="amount" value="8.00" />
+                                                        <input type="hidden" name="discount_amount" value="1.00" />
+                                                        <input type="hidden" name="currency_code" value="USD" />
+                                                        <input type="hidden" name="return" value=" " />
+                                                        <input type="hidden" name="cancel_return" value=" " />
+                                                        <input type="submit" name="submit" value="Add to cart" class="button" />
+                                                    </fieldset>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </figure>
+                                </div>
+                            </div>
+                            </div>
+                        </div>
+                    <?php } ?>
+					<div class="clearfix"> </div>
+				</div>
+			</div>
+		</div>
+		<div class="clearfix"></div>
+	</div>
+<!-- //banner -->

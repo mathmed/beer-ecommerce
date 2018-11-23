d3c5b4fd76397d952584c3a389beeeb5135a02b9
eduviictor <eduvictornobrega@gmail.com>
2018-11-23 00:38:45 -0300

    Começando a ajustar o responsivo do header

--- application/views/public/header.php
+++ application/views/public/header.php
@@ -104,10 +104,10 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 			</div>
 			<div class="w3ls_logo_products_left1">
 				<ul class="special_items">
-					<li><a href="/beer-ecommerce/events">Events</a><i>/</i></li>
-					<li><a href="/beer-ecommerce/about">About Us</a><i>/</i></li>
-					<li><a href="/beer-ecommerce/products">Best Deals</a><i>/</i></li>
-					<li><a href="/beer-ecommerce/services">Services</a></li>
+					<li><a href="/beer-ecommerce/events">Eventos</a><i>/</i></li>
+					<li><a href="/beer-ecommerce/about">Sobre</a><i>/</i></li>
+					<li><a href="/beer-ecommerce/products">Produtos</a><i>/</i></li>
+					<li><a href="/beer-ecommerce/services">Serviços</a></li>
 				</ul>
 			</div>
 			<div class="w3ls_logo_products_left1">

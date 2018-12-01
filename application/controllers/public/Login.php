d61c71483d512fe40f4e205aca7809bebf184ab8
laioAndrade <laioand98@gmail.com>
2018-11-30 22:03:31 -0200

    Configurando o controller Login.php

--- application/controllers/public/Login.php
+++ application/controllers/public/Login.php
@@ -10,16 +10,32 @@ Class Login extends CI_Controller{
         /* carregando os models e DAOs necessários */
 		parent::__construct();
         $this->load->dao("categoria_dao", "", TRUE);
+        $this->load->dao("auth_dao", "", TRUE);
     }
 
     public function index(){
         $data["dados"] = $this->categoria_dao->getCategorias();
+
         $this->load->view('public/header.php');
         $this->load->view("public/login.php", $data);
         $this->load->view('public/footer.php');
     }
+    
+    public function logar(){
+        $dados['senha'] = $this->input->post("senha");
+        $dados['cpf'] = $this->input->post("cpf");
+        $dados['tipo_auth'] = $this->input->post("tipo_auth");
+        if($dados['cpf'] && $dados['senha']) $auth = $this->auth_dao->auth($dados);
+    }
+
+    public function deslogar(){
+        $this->auth_dao->logout();
+
+    }
 
 }
+
+
 
 
 ?>
